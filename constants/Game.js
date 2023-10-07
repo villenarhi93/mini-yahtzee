5cc24c12834ca4e32254dd89209b466a7de83d9f
Ville Närhi <villenarhi@hotmail.fi>
2023-10-07 15:27:10 +0300

    added files

---
+++ constants/Game.js
@@ -0,0 +1,7 @@
+export const NBR_OF_DICES = 5;
+export const NBR_OF_THROWS = 3; 
+export const MIN_SPOT = 1;
+export const MAX_SPOT = 6;
+export const BONUS_POINTS_LIMIT = 63;
+export const BONUS_POINTS = 50;
+export const NBR_OF_SCOREBOARD = 5;
